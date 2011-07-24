d918cfe5d9e7d5701404f8761c762e73d4715380
Rudy Gevaert <rudy+github@webworm.org>
2011-07-24 22:49:08 +0200

    Manage mysql dirs

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,20 @@
 class mysql::config
 {
+  file {
+    "/etc/mysql/":
+      ensure  => directory,
+      owner   => root,
+      group   => root,
+      mode    => 755;
+    "/etc/mysql/conf.d/":
+      ensure  => directory,
+      owner   => root,
+      group   => root,
+      mode    => 755;
+    "/var/lib/mysql":
+      ensure  => directory,
+      owner   => mysql,
+      group   => mysql,
+      mode    => 700;
+  }
 }
