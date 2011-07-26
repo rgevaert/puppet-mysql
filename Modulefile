d6173c895c426f46e43198e9445d69d7ac11cda5
rgevaert <rudy.gevaert+github@ugent.be>
2011-07-26 09:59:34 +0200

    Describe module to be used with puppet-module tool.

---
+++ Modulefile
@@ -0,0 +1,11 @@
+name    'rgevaert-mysql'
+version '0.0.1'
+source 'https://github.com/rgevaert/puppet-mysql'
+author 'rgevaert'
+license 'GPL v3 or later'
+summary 'Manage your percona, mariadb or oracle MySQL'
+description 'UNKNOWN'
+project_page 'UNKNOWN'
+
+## Add dependencies, if any:
+# dependency 'camptocamp/apt', '>= 0.0.1'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,21 @@
+# Class: mysql
+#
+# This module manages mysql.  It gives you the choice mysql
+# server you want to use: oracle, mariadb or percona
+#
+# Parameters:
+# - type: oracle|mariadb|percona
+#
+# Actions:
+#
+# Requires: camptocamp/apt rgevaert/pwgen
+#
+# Sample Usage:
+# class {
+#  mysql:
+#    type => mariadb
+# }
+#
 class mysql ( $type='oracle')
 {
   include mysql::params
