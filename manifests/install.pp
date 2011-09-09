2ad38822dd60fef7afcde5d746b23107e52d894c
Rudy Gevaert <rudy.gevaert@ugent.be>
2011-09-09 10:43:21 +0200

    Add script to set secure mysql.

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,4 +6,13 @@ class mysql::install
     $mysql::params::packages_extra:
      ensure => installed;
   }
+
+  file {
+    '/usr/local/bin/mysql_secure_installation_socket':
+      ensure  => present,
+      owner   => root,
+      group   => root,
+      mode    => 755,
+      source  => [ "puppet:///modules/mysql/mysql_secure_installation_socket" ];
+  }
 }
