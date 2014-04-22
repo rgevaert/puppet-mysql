73d46dcc9495782a8a1669f0e743c56478091997
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2014-04-22 15:08:25 +0200

    puppet-lint fixes

--- tests/repos.pp
+++ tests/repos.pp
@@ -2,19 +2,19 @@ class repos
 {
   apt::sources_list {"debian-de":
     ensure  => present,
-    content => "deb http://ftp.de.debian.org/debian/ ${lsbdistcodename} main non-free contrib";
+    content => "deb http://ftp.de.debian.org/debian/ ${::lsbdistcodename} main non-free contrib";
   }
 
   apt::sources_list{"security":
        ensure  => present,
-       content => "deb http://security.debian.org/ ${lsbdistcodename}/updates main non-free contrib";
+       content => "deb http://security.debian.org/ ${::lsbdistcodename}/updates main non-free contrib";
   }
 
   apt::sources_list {"backports":
         ensure  => present,
-        content => $operatingsystem ? {
-            "Debian"   => "deb http://backports.debian.org/debian-backports ${lsbdistcodename}-backports main non-free",
-            "Ubuntu"   => "deb http://be.archive.ubuntu.com/ubuntu/ ${lsbdistcodename}-backports main restricted universe multiverse",
+        content => $::operatingsystem ? {
+            "Debian"   => "deb http://backports.debian.org/debian-backports ${::lsbdistcodename}-backports main non-free",
+            "Ubuntu"   => "deb http://be.archive.ubuntu.com/ubuntu/ ${::lsbdistcodename}-backports main restricted universe multiverse",
         }
     }
 }
