0b036824a0871b372d624830fe7aca3db91f87be
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2014-04-23 12:33:22 +0200

    fix WARNING: top-scope variable being used without an explicit namespace

--- manifests/functions.pp
+++ manifests/functions.pp
@@ -1,71 +1,72 @@
 class mysql::functions
 {
-  define database ( $ensure, $dump = NONE )
-  {
-    case $ensure {
-      present: {
-        exec { "MySQL: create $name db":
-          command => "/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf --execute=\"CREATE DATABASE ${name}\";",
-          unless  => "/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf --execute=\"SHOW DATABASES;\" | grep -x '${name}'",
-          require => Class['mysql::install'],
-        }
-      }
+}
 
-      importdb: {
-        exec { 'MySQL: import db':
-          command   => "/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf --execute=\"CREATE DATABASE ${name}\";
-                /usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf ${name} < ${dump}",
-          require   => Class['mysql::install'],
-        }
-      }
-
-      absent: {
-        exec { "MySQL: drop ${name} db":
-          command => "/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf --execute=\"DROP DATABASE ${name}\";",
-          onlyif  => "/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf --execute=\"SHOW DATABASES;\" | grep -x '${name}'",
-          require => Class['mysql::install'],
-        }
-      }
-
-      default: {
-        fail "Invalid 'ensure' value '${ensure}' for mysql::database"
+define mysql::functions::database ( $ensure, $dump = NONE )
+{
+  case $ensure {
+    present: {
+      exec { "MySQL: create $name db":
+        command => "/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf --execute=\"CREATE DATABASE ${name}\";",
+        unless  => "/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf --execute=\"SHOW DATABASES;\" | grep -x '${name}'",
+        require => Class['mysql::install'],
       }
     }
-  }
 
-  define user ( $password, $database, $host='localhost' )
-  {
-    exec { "MySQL: create user ${name}":
-      command => "/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf --execute=\"GRANT ALL PRIVILEGES ON ${database}.* TO '${name}'@'${host}' IDENTIFIED BY '${password}' WITH GRANT OPTION;\";",
-      require => Class['mysql::install'],
-      unless  => "mysql --user=${name} --password=${password} --database=${database} --host=${host}",
+    importdb: {
+      exec { 'MySQL: import db':
+        command   => "/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf --execute=\"CREATE DATABASE ${name}\";
+              /usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf ${name} < ${dump}",
+        require   => Class['mysql::install'],
+      }
+    }
+
+    absent: {
+      exec { "MySQL: drop ${name} db":
+        command => "/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf --execute=\"DROP DATABASE ${name}\";",
+        onlyif  => "/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf --execute=\"SHOW DATABASES;\" | grep -x '${name}'",
+        require => Class['mysql::install'],
+      }
+    }
+
+    default: {
+      fail "Invalid 'ensure' value '${ensure}' for mysql::database"
     }
   }
+}
 
-  define conf ($ensure, $config = $name)
-  {
-    case $ensure {
-      present: {
-        file { "/etc/mysql/conf.d/${name}.cnf":
-          ensure  => present,
-          owner   => 'root',
-          group   => 'root',
-          mode    => '0600',
-          content => template ("mysql/${name}.cnf.erb"),
-          require => Class['config'],
-        }
-      }
+define mysql::functions::user ( $password, $database, $host='localhost' )
+{
+  exec { "MySQL: create user ${name}":
+    command => "/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf --execute=\"GRANT ALL PRIVILEGES ON ${database}.* TO '${name}'@'${host}' IDENTIFIED BY '${password}' WITH GRANT OPTION;\";",
+    require => Class['mysql::install'],
+    unless  => "mysql --user=${name} --password=${password} --database=${database} --host=${host}",
+  }
+}
 
-      absent: {
-        file { "/etc/mysql/conf.d/${name}.cnf":
-          ensure  => absent,
-          require => Class['config'],
-        }
+define mysql::functions::conf ($ensure, $config = $name)
+{
+  case $ensure {
+    present: {
+      file { "/etc/mysql/conf.d/${name}.cnf":
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0600',
+        content => template ("mysql/${name}.cnf.erb"),
+        require => Class['config'],
       }
+    }
 
-      default: {
-        fail ("Unknown ensure value ${ensure}")
+    absent: {
+      file { "/etc/mysql/conf.d/${name}.cnf":
+        ensure  => absent,
+        require => Class['config'],
       }
+    }
+
+    default: {
+      fail ("Unknown ensure value ${ensure}")
     }
   }
 }
