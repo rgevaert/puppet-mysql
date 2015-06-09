874cfad0b34cf13902f8a9538aeb928600ded1c5
Rudy Gevaert <rudy@webworm.org>
2015-06-09 11:28:07 +0200

    Support overriding of packages list

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,7 +23,7 @@ class mysql ( $type                           = $mysql::params::type,
               $multi_initscript               = 'puppet:///modules/mysql/init.multi',
               $multi_create_instance_script   = 'puppet:///modules/mysql/create_instance',
               $package_ensure                 = $mysql::params::package_ensure,
-              $manage_repo                    = true
+              $manage_repo                    = true,
               $packages                       = '')
 inherits mysql::params {
 
